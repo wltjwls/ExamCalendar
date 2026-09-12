e4afbc76f616a0cb90ea09ce892d4defab4bfb5f
wltjwls <wltjwlsy@gmail.com>
2026-09-13 01:17:13 +0900

    Initial Commit

---
+++ ExamCalendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExamCalendar
+//
+//  Created by 뚱땅E on 9/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExamCalendar/ExamCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamCalendarApp.swift
+//  ExamCalendar
+//
+//  Created by 뚱땅E on 9/13/26.
+//
+
+import SwiftUI
+
+@main
+struct ExamCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
